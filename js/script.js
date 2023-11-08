097b5e42d87ab00d591a5b43542e9eddd86d7044
Sandra <145571922+Sandrawebit@users.noreply.github.com>
2023-11-08 15:56:19 +0200

    е

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
-const tabsBtns = document.querySelectorAll(".tabs-nav button");
-//console.log(tabsBtns);
-const tabsItems = document.querySelectorAll(".tabs-item");
-// console.log(tabsItems);
+document.addEventListener("DOMContentLoaded", ()=>{
+    init()
+})
 
-//Ф-я скрывает табы и убирает active у кнопок
-function hideTabs() {
-    tabsItems.forEach(item => item.classList.add("hide"));
-    tabsBtns.forEach(item => item.classList.remove("active"));
-}
 
-//Ф-я показывает переданный номер таба и делает соответствующую ему кнопку активной.
-function showTab(index) {
-    tabsItems[index].classList.remove("hide");
-    tabsBtns[index].classList.add("active");
-}
+// Burger menu
 
-hideTabs();
-showTab(0);
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
 
-tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
-    hideTabs();
-    showTab(index);
-}));
+})
